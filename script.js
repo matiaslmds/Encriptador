a72c1e4bc27931cbab66260dbb6d4f0fca95c714
Matias <matiasbtvs@hotmail.com>
2023-01-30 00:00:17 -0300

    Primer commit proyecto casi terminado

---
+++ script.js
@@ -0,0 +1,53 @@
+const textArea = document.querySelector(".texto-ingresado");
+const mensaje = document.querySelector(".texto-encriptado");
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function botonEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+//** ENCRIPTAR **//
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for (let i = 0 ; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+        }
+    }
+    return stringEncriptada
+}
+
+//** DESENCRIPTAR **//
+
+
+function botonDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+
+}
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for (let i = 0 ; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll( matrizCodigo[i][1], matrizCodigo[i][0])
+
+        }
+    }
+    return stringDesencriptada
+}
+
+
